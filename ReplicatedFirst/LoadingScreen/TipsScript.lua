7d12e5cd27196ad3bbc600ca928fe39a495b1662
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-02-26 19:24:01 +1000

    Create TipsScript.lua

---
+++ ReplicatedFirst/LoadingScreen/TipsScript.lua
@@ -0,0 +1,16 @@
+local Tip = script.Parent
+
+while true do
+	wait(5)
+	Tip.Text = "The higher the acceleration value, the better you move..."
+	wait(5)
+	Tip.Text = "You can spend some blood to increase the acceleration value [F]..."
+	wait(5)
+	Tip.Text = "If the acceleration value is maximal, chop everyone into mincemeat! [Q]..."
+	wait(5)
+	Tip.Text = "If you don't have much blood left, turn off the chainsaw [E]..."
+	wait(5)
+	Tip.Text = "Don't forget about attacks! [LMB]..."
+	wait(5)
+	Tip.Text = "If the acceleration value is more than half, you can use the double jump..."
+end
